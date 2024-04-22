e3539b05b344f7fc0c2eae4e50fea88c22897090
awaissafdar413 <159916432+awaissafdar413@users.noreply.github.com>
2024-04-22 20:28:26 +0500

    rrrrrrrrrrrr

--- database/migrations/2024_04_16_155923_create_emaildatas_table.php
+++ database/migrations/2024_04_16_155923_create_emaildatas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('emaildatas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('Name');
             $table->string('email');
             $table->timestamps();
         });
